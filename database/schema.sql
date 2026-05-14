ac558b9203a994a7702fa52cea7002091cbed556
Guilherme Maciel <guilherme0maciel3@gmail.com>
2026-05-13 22:55:50 -0300

    Delete database/schema.sql
    Arquivo duplicado

--- database/schema.sql
+++
@@ -1,7 +0,0 @@
-USE concessionaria_db;
-
--- Corrige a coluna chassi para aceitar qualquer tamanho e não ser unique obrigatório
-ALTER TABLE veiculos MODIFY chassi VARCHAR(50);
-ALTER TABLE veiculos MODIFY placa VARCHAR(20);
-
-SELECT 'Atualização concluída!' as status;
